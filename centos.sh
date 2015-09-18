37b9318cfacb135f24586f90c92375acca4af06f
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-09-17 19:22:07 -0700

    yada-yada

--- centos.sh
+++ centos.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+curl -O https://raw.githubusercontent.com/TaylorMonacelli/emacs-in-containers/wip/init.sh
+
+distro=centos
+distro=fedora
+distro=ubuntu
+
 dt=$(date +%s)
-name=centos_$dt
+name=${distro}_$dt
 docker rm $name 2>/dev/null
-docker run --name $name -v `pwd`:/root -it centos sh -c 'exec sh -x /root/init.sh'
+docker run --name $name -v `pwd`:/root -it $distro sh -c 'exec sh -x /root/init.sh'
