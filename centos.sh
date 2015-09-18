be04ced4ad70f79bba9f95ff6e79f236a9d58766
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-09-17 19:23:50 -0700

    yada-yada

--- centos.sh
+++ centos.sh
@@ -8,5 +8,7 @@ dt=$(date +%s)
 name=${distro}_$dt
 
 curl -O https://raw.githubusercontent.com/TaylorMonacelli/emacs-in-containers/wip/init.sh
+curl -O https://raw.githubusercontent.com/TaylorMonacelli/emacs-in-containers/wip/stow.sh
+curl -O https://raw.githubusercontent.com/TaylorMonacelli/emacs-in-containers/wip/emacs.sh
 docker rm $name 2>/dev/null
-docker run --name $name -v `pwd`:/root -it $distro sh -c 'exec sh -x /root/init.sh'
+docker run --name $name -v `pwd`:/root -it $distro sh -c 'exec sh -x /root/init.sh; sh -x /root/stow.sh'
