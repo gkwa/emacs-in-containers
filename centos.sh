041e7380284e7093bc75397db1897f645f9249c3
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-09-17 19:01:56 -0700

    yada-yada

--- centos.sh
+++ centos.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
-if yum --version >/dev/null 2>&1; then yum install curl; else apt-get -y -qq update; apt-get -y -qq install curl; fi
+if dnf --version >/dev/null 2>&1
+then 
+	dnf install curl
+
+elif yum --version >/dev/null 2>&1
+then
+	yum install curl
+
+elif apt-get --version >/dev/null 2>&1
+then
+	apt-get -y -qq update
+	apt-get -y -qq install curl
+fi
+
+# stow
 curl -o stow-install.sh https://raw.githubusercontent.com/TaylorMonacelli/gnu-stow-install/master/install.sh
 sh -x stow-install.sh
+
+# emacs
 curl -o emacs-install-generic.sh https://raw.githubusercontent.com/TaylorMonacelli/emacs_build/emacsgeneric/emacs-install-generic.sh
 sh -x emacs-install-generic.sh
