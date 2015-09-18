e666eaaad16fd6b0dea775147596afef8443e804
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-09-17 19:06:38 -0700

    yada-yada

--- centos.sh
+++ centos.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+dt=$(date +%s)
+name=centos_$dt
+docker rm $name 2>/dev/null
+docker run --name $name -v `pwd`:/root -it centos sh -c 'exec sh -x /root/centos.sh'
+
 if dnf --version >/dev/null 2>&1
 then 
 	dnf install curl
