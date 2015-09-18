a2ef1a4f37f6b55fe83ddf8f6f9094ae64dcfc43
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-09-17 20:53:44 -0700

    yada-yada

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,6 +4,9 @@ cd ~/
 git init
 git remote rm origin 2>/dev/null
 
-git remote add origin git@github.com:taylormonacelli/dotfiles.git
+git remote add origin https://github.com/taylormonacelli/dotfiles.git
 git fetch --prune
 git checkout --track --force origin/master
+
+git remote rm origin
+git remote add origin git@github.com:taylormonacelli/dotfiles.git
