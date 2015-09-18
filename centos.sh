a13d7babd5543bffde46ed5926df35feb45e1684
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-09-17 19:07:46 -0700

    yada-yada

--- centos.sh
+++ centos.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+: <<COMMENTBLOCK
 dt=$(date +%s)
 name=centos_$dt
 docker rm $name 2>/dev/null
 docker run --name $name -v `pwd`:/root -it centos sh -c 'exec sh -x /root/centos.sh'
+COMMENTBLOCK
 
 if dnf --version >/dev/null 2>&1
 then 
